ff910ff8d50009e7a9b3ef06c4b3d607f5528754
Tao <defualt@qq.com>
2018-03-15 15:19:47 +0800

    modified:   .idea/modules.xml 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/mg/studio/weatherappdesign/MainActivity.java 	new file:   app/src/main/res/anim/tip.xml 	new file:   app/src/main/res/drawable/app_icon.png 	new file:   app/src/main/res/drawable/circle_shape.xml 	new file:   app/src/main/res/drawable/refresh_button.png 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -3,8 +3,14 @@ package mg.studio.weatherappdesign;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,18 +20,48 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imageButton;
+    private TextView weekText;
+    private TextView dateText;
+
+    private Animation operatingAnim;
+
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAll();
+    }
+
+    private void initAll(){
+        //init View
+        imageButton=(ImageButton)findViewById(R.id.refresh_button);
+        weekText=(TextView)findViewById(R.id.weekText);
+        dateText=(TextView)findViewById(R.id.tv_date);
+
+        //init Animation
+        operatingAnim= AnimationUtils.loadAnimation(this,R.anim.tip);
+        LinearInterpolator lin=new LinearInterpolator();
+        operatingAnim.setInterpolator(lin);
+
+        //init calendar
+        calendar=Calendar.getInstance();
     }
 
     public void btnClick(View view) {
         new DownloadUpdate().execute();
+
+        // start animation
+        imageButton.startAnimation(operatingAnim);
+
     }
 
 
@@ -84,6 +120,48 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String temperature) {
             //Update the temperature displayed
             ((TextView) findViewById(R.id.temperature_of_the_day)).setText(temperature);
+
+            //end animation
+            imageButton.clearAnimation();
+
+            //update time
+            updateCalender();
         }
+    }
+
+    private void updateCalender(){
+        String year =Integer.toString(calendar.get(Calendar.YEAR));
+        String month =Integer.toString(calendar.get(Calendar.MONTH)+1);
+        String day =Integer.toString(calendar.get(Calendar.DAY_OF_MONTH));
+        int week=calendar.get(Calendar.DAY_OF_WEEK);
+
+        String strWeek="";
+        switch (week){
+            case Calendar.SUNDAY:
+                strWeek="SUNDAY";
+                break;
+            case Calendar.MONDAY:
+                strWeek="MONDAY";
+                break;
+            case Calendar.TUESDAY:
+                strWeek="TUESDAY";
+                break;
+            case Calendar.WEDNESDAY:
+                strWeek="WEDNESDAY";
+                break;
+            case Calendar.THURSDAY:
+                strWeek="THURSDAY";
+                break;
+            case Calendar.FRIDAY:
+                strWeek="FRIDAY";
+                break;
+            case Calendar.SATURDAY:
+                strWeek="SATURDAY";
+                break;
+            default:
+                break;
+        }
+        dateText.setText(day+'/'+month+'/'+year);
+        weekText.setText(strWeek);
     }
 }
